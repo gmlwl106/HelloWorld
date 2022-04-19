7d550606dd121f10dfdb5fc9f1fbc0c9eef8b2ca
gmlwl106 <66778894@naver.com>
2022-04-19 17:32:33 +0900

    HelloWorld. 마름모

--- src/com/javaex/heeji/Ex04.java
+++ src/com/javaex/heeji/Ex04.java
@@ -12,21 +12,28 @@ public class Ex04 {
 		int num = sc.nextInt();
 		
 		//마름모
-		for(int i=1; i<=(num+(num-1)); i++) {
-			if(i <= num) {
-				for(int j=1; j<=(num-i); j++) {
-					System.out.print(" ");
-				}
-				for(int j=1; j<=(i+(i-1)); j++) {
-					System.out.print("*");
-				}
-				System.out.println();
-			} else {
-				for(int j=1; j<=(i-num); j++) {
-					System.out.print(" ");
-				}
-				
+		
+		//마름모 윗부분
+		for(int i=1; i<=num; i++) {
+			for(int j=1; j<=(num-i); j++) {
+				System.out.print(" ");
 			}
+			for(int j=1; j<=(2*i-1); j++) {
+				System.out.print("*");
+			}
+			System.out.println();
 		}
+		//마름모 아랫부분
+		for(int i=(num-1); i>0; i--) {
+			for(int j=(num-i); j>0; j--) {
+				System.out.print(" ");
+			}
+			for(int j=(2*i-1); j>0; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+		sc.close();
 	}
 }
