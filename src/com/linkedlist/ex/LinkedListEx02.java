ff18740992c14e713ae73153a2549e4fc50bfa43
gmlwl106 <66778894@naver.com>
2022-05-15 18:11:30 +0900

    2022.05.15. LinkedList 구현2 (첫번째노드 삭제하게 함)

--- src/com/linkedlist/ex/LinkedListEx02.java
+++ src/com/linkedlist/ex/LinkedListEx02.java
@@ -3,6 +3,54 @@ package com.linkedlist.ex;
 //LinkedList의 Node header를 만들어서
 //첫번째 노드가 삭제되지 않는 문제를 해결
 public class LinkedListEx02 {
+	
+	static class LinkedList {
+		Node header;
+		
+		class Node {
+			int data;
+			Node next = null;
+		}
+		
+		LinkedList() {
+			header = new Node();
+		}
+
+		//node 추가
+		public void append(int d) {
+			Node end = new Node();
+			end.data = d;
+			Node n = header; //시작노드
+			while(n.next != null) {
+				n = n.next;
+			}
+			n.next = end;
+		}
+		
+		//node 삭제 ->첫번째 노드는 삭제되지 않는 코드
+		public void delete(int d) {
+			Node n = header; //시작노드
+			while(n.next != null) {
+				if(n.next.data == d) {
+					n.next = n.next.next;
+				} else {
+					n = n.next;
+				}
+			}
+		}
+		
+		//현재 노드들을 출력
+		void retrieve() {
+			Node n = header.next; //시작노드
+			while(n.next != null) {
+				System.out.print(n.data+"->");
+				n=n.next;
+			}
+			
+			System.out.println(n.data);
+		}
+	}
+	
 	public static void main(String[] args) {
 		LinkedList ll = new LinkedList();
 		ll.append(1);
@@ -19,49 +67,3 @@ public class LinkedListEx02 {
 	
 }
 
-class LinkedList {
-	Node header;
-	
-	static class Node {
-		int data;
-		Node next = null;
-	}
-	
-	LinkedList() {
-		header = new Node();
-	}
-
-	//node 추가
-	public void append(int d) {
-		Node end = new Node();
-		end.data = d;
-		Node n = header; //시작노드
-		while(n.next != null) {
-			n = n.next;
-		}
-		n.next = end;
-	}
-	
-	//node 삭제 ->첫번째 노드는 삭제되지 않는 코드
-	public void delete(int d) {
-		Node n = header; //시작노드
-		while(n.next != null) {
-			if(n.next.data == d) {
-				n.next = n.next.next;
-			} else {
-				n = n.next;
-			}
-		}
-	}
-	
-	//현재 노드들을 출력
-	void retrieve() {
-		Node n = header.next; //시작노드
-		while(n.next != null) {
-			System.out.print(n.data+"->");
-			n=n.next;
-		}
-		
-		System.out.println(n.data);
-	}
-}
