74f4285d41a225f867cbdddc08430f10574ffe5b
gmlwl106 <66778894@naver.com>
2022-05-15 18:12:53 +0900

    2022.05.15. LinkedList 구현3 (중복값 제거)

--- src/com/linkedlist/ex/LinkedListEx03.java
+++ src/com/linkedlist/ex/LinkedListEx03.java
@@ -2,8 +2,74 @@ package com.linkedlist.ex;
 
 
 public class LinkedListEx03 {
+	
+	static class LinkedList {
+		Node header;
+		
+		static class Node {
+			int data;
+			Node next = null;
+		}
+		
+		LinkedList() {
+			header = new Node();
+		}
+
+		//node 추가
+		public void append(int d) {
+			Node end = new Node();
+			end.data = d;
+			Node n = header; //시작노드
+			while(n.next != null) {
+				n = n.next;
+			}
+			n.next = end;
+		}
+		
+		//node 삭제 ->첫번째 노드는 삭제되지 않는 코드
+		public void delete(int d) {
+			Node n = header; //시작노드
+			while(n.next != null) {
+				if(n.next.data == d) {
+					n.next = n.next.next;
+				} else {
+					n = n.next;
+				}
+			}
+		}
+		
+		//현재 노드들을 출력
+		void retrieve() {
+			Node n = header.next; //시작노드
+			while(n.next != null) {
+				System.out.print(n.data+"->");
+				n=n.next;
+			}
+			
+			System.out.println(n.data);
+		}
+		
+		//포인터를 이용해서 중복값 삭제
+		//r이 움직이면서 중복값이 있는지 확인한다
+		void removeDups() {
+			Node n = header;
+			while(n.next != null) {
+				Node r = n;
+				while(n != null && r.next != null) {
+					if(n.data == r.next.data) {
+						r.next = r.next.next;
+					} else {
+						r = r.next;
+					}
+				}
+				
+				n = n.next;
+			}
+		}
+	}
+	
 	public static void main(String[] args) {
-		LinkedList2 ll = new LinkedList2();
+		LinkedList ll = new LinkedList();
 		ll.append(1);
 		ll.append(2);
 		ll.append(3);
@@ -23,66 +89,5 @@ public class LinkedListEx03 {
 	
 }
 
-class LinkedList2 {
-	Node header;
-	
-	static class Node {
-		int data;
-		Node next = null;
-	}
-	
-	LinkedList2() {
-		header = new Node();
-	}
 
-	//node 추가
-	public void append(int d) {
-		Node end = new Node();
-		end.data = d;
-		Node n = header; //시작노드
-		while(n.next != null) {
-			n = n.next;
-		}
-		n.next = end;
-	}
-	
-	//node 삭제 ->첫번째 노드는 삭제되지 않는 코드
-	public void delete(int d) {
-		Node n = header; //시작노드
-		while(n.next != null) {
-			if(n.next.data == d) {
-				n.next = n.next.next;
-			} else {
-				n = n.next;
-			}
-		}
-	}
-	
-	//현재 노드들을 출력
-	void retrieve() {
-		Node n = header.next; //시작노드
-		while(n.next != null) {
-			System.out.print(n.data+"->");
-			n=n.next;
-		}
-		
-		System.out.println(n.data);
-	}
-	
-	void removeDups() {
-		Node n = header;
-		while(n.next != null) {
-			Node r = n;
-			while(n != null && r.next != null) {
-				if(n.data == r.next.data) {
-					r.next = r.next.next;
-				} else {
-					r = r.next;
-				}
-			}
-			
-			n = n.next;
-		}
-	}
-}
 
